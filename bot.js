f35b7f353afe1d648c98031fe26670133f876858
CaesiumSG <69115162+CaesiumSG@users.noreply.github.com>
2020-08-06 22:51:21 +0800

    xp

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const util = require('util');
 const { Client, MessageEmbed, Permissions } = require('discord.js');
 const client = new Client();
 const Discord = require('discord.js');
-
+let db = JSON.parse(fs.readFileSync("./database.json", "utf8"));
 const config = require('./config.json');
 const guilds = require('./guilds.json');
 const fs = require('fs');
@@ -1056,3 +1056,40 @@ client.on('message', message => {
 		message.channel.send(util.inspect(roleFinalPermissions.serialize()), { code: 'js' });
 	}
 });
+
+client.on("message", message => {
+    if (message.author.bot) return; // ignore bots
+
+    // if the user is not on db add the user and change his values to 0
+    if (!db[message.author.id]) db[message.author.id] = {
+        xp: 0,
+        level: 0
+      };
+    db[message.author.id].xp++;
+    let userInfo = db[message.author.id];
+    if(userInfo.xp > 100) {
+        userInfo.level++
+        userInfo.xp = 0
+        message.reply("Congratulations comrade, you became 1 level better at being a socialist")
+    }
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if(cmd === "c!xp") {
+        let userInfo = db[message.author.id];
+        let member = message.mentions.members.first();
+        let embed = new Discord.RichEmbed()
+        .setColor(0x4286f4)
+        .addField("Level", userInfo.level)
+        .addField("XP", userInfo.xp+"/100");
+        if(!member) return message.channel.sendEmbed(embed)
+        let memberInfo = db[member.id]
+        let embed2 = new Discord.RichEmbed()
+        .setColor(0x4286f4)
+        .addField("Level", memberInfo.level)
+        .addField("XP", memberInfo.xp+"/100")
+        message.channel.sendEmbed(embed2)
+    }
+    fs.writeFile("./database.json", JSON.stringify(db), (x) => {
+        if (x) console.error(x)
+      });
+})
