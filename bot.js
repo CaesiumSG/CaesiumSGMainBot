85058fe20c19364c2a6213f755a0def990e7774f
CaesiumSG <69115162+CaesiumSG@users.noreply.github.com>
2020-08-08 22:55:28 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 const util = require('util');
 const { Client, MessageEmbed, Permissions, Collection } = require('discord.js');
-
-const Discord = require('discord.js');
-
 const client = new Client();
+const Discord = require('discord.js');
 
 const config = require('./config.json');
 const guilds = require('./guilds.json');
@@ -17,7 +15,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	client.command.set(command.name, command);
+	client.commands.set(command.name, command);
 }
 
 
@@ -28,62 +26,25 @@ require('events').EventEmitter.defaultMaxListeners = 180;
 client.login(process.env.token);
 console.log("bot client has logged in")
 
-const cooldowns = new Discord.Collection();
 
 
 
-client.on('message', message => {
+client.on('message', message => { //modular selection
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const commandName = args.shift().toLowerCase();
+	const command = args.shift().toLowerCase();
 
-	const command = client.commands.get(commandName)
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-	if (!command) return;
-
-	if (command.guildOnly && message.channel.type !== 'text') {
-		return message.reply('I can\'t execute that command inside DMs!');
-	}
-
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-
-		return message.channel.send(reply);
-	}
-
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
-
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-		}
-	}
-
-	timestamps.set(message.author.id, now);
-	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	if (!client.commands.has(command)) return;
 
 	try {
-		command.execute(message, args);
+		client.commands.get(command).execute(message, args);
 	} catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
 	}
 });
+
 
 client.on('message', async message => { //verification
     
@@ -1096,3 +1057,4 @@ client.on('message', message => {
 		message.channel.send(util.inspect(roleFinalPermissions.serialize()), { code: 'js' });
 	}
 });
+
